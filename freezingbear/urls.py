88920d9e739e1f81f41737e72d867b05ed49e693
michael truong <truongmt@gmail.com>
2013-12-23 15:46:44 -0800

    basic accounts profile page

--- freezingbear/urls.py
+++ freezingbear/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
+from django.views.generic import TemplateView
+
 from django.contrib import admin
 admin.autodiscover()
 
@@ -10,4 +12,5 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.simple.urls')),
+    (r'^accounts/profile', TemplateView.as_view(template_name='profile/profile.html')),
 )
