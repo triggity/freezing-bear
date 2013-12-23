cced684ac69252cf4f44a637a6245aee671d5bd7
michael truong <truongmt@gmail.com>
2013-12-22 20:47:59 -0800

    adding static files, basecss

--- freezingbear/settings.py
+++ freezingbear/settings.py
@@ -88,8 +88,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = os.path.join(BASE_DIR, 'static/')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 #Django-bootstrap3 settings, for offline
 BOOTSTRAP3 = {
   'base_url': 'http://localhost/bootstrap3/',
