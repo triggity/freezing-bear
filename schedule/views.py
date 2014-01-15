c44d2c0175def2f2243d55599d6184993c714ff6
michael truong <truongmt@gmail.com>
2014-01-14 23:03:38 -0800

    dummy table inserted

--- schedule/views.py
+++ schedule/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 def index(req):
-    return render(req, 'schedule/index.html')
+  return render(req, 'schedule/index.html', {'days':days, 'distance': range(10)})
 
