195f0d21077cdf596fd7022acf3cddd44da679ac
michael truong <truongmt@gmail.com>
2014-01-28 23:12:11 -0800

    adding requirejs

---
+++ static/js/main.js
@@ -0,0 +1,23 @@
+
+require.config({
+//  paths: {
+//    'underscore': 'vendor/underscore/underscore',
+//    'backbone': 'vendor/backbone/backbone',
+//    'jquery': 'vendor/jquery/jquery-2.0.3.min',
+//  
+//  },
+//  shim: {
+//    "underscore": {
+//      "exports": "_"
+//    },
+//    "backbone": {
+//      "deps": ["underscore", "jquery"],
+//      "exports": "Backbone"
+//    }
+//  }
+});
+require([
+  ], function() {
+  
+  console.log('run require'); 
+  });
