0f318705240a7675e789eddb5fe54cfd28f7b78a
michael truong <truongmt@gmail.com>
2014-01-28 22:36:46 -0800

    table in js instead

--- schedule/models.py
+++ schedule/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 day_choices = (
     ('Monday', 'Mon'),
@@ -14,3 +15,4 @@ class Choices(models.Model):
     day = models.CharField(max_length=9, choices=day_choices)
     block_start = models.IntegerField(default=0)
     block_length = models.IntegerField(default=1)
+    #user = models.ForeignKey(User)
