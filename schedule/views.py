068d9e035402b85efeb10a78366a6b841c7e220c
michael truong <truongmt@gmail.com>
2014-02-02 22:45:29 -0800

    more table

--- schedule/views.py
+++ schedule/views.py
@@ -1,12 +1,16 @@
+import json
 from django.shortcuts import render
 from django.http import HttpResponse
-days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 blocks = range(24)
 def index(req):
+  props = [["{} {}".format(day, block) for day in days] for block in blocks]
   payload = {
       'days': days,
-      'blocks': blocks
+      'blocks': blocks,
+      'table_data': json.dumps(props)
   }
+  print props
   people = [{'id': 1, 'name' : 'foo'}, {'id': 2, 'name' : 'bar'}]
   return render(req, 'schedule/index.html', payload)
 
