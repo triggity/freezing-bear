ad9e9ed2d89f7823f0bf71b39d6144c5b587efea
michael truong <truongmt@gmail.com>
2014-02-04 18:52:00 -0800

    table can receive model and display form

--- freezingbear/urls.py
+++ freezingbear/urls.py
@@ -26,4 +26,3 @@ def show_urls(urllist, depth=0):
         if hasattr(entry, 'url_patterns'):
             show_urls(entry.url_patterns, depth + 1)
 
-show_urls(urlpatterns)
--- schedule/views.py
+++ schedule/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render
 from django.http import HttpResponse
 import itertools
+from schedule.models import Choices
 days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
 blocks = range(24)
 props = [["{} {}".format(day, block) for day in days[2:4] for block in blocks[11:16]]]
@@ -9,15 +10,21 @@ chain = list(itertools.chain(*props))
 def index(req):
     payload = {
         'days': days,
-        'blocks': blocks,
-        'table_data': json.dumps(chain)
+        'blocks': blocks
     }
     return render(req, 'schedule/index.html', payload)
 
 from restless.views import Endpoint
+from django.core import serializers
 
 class scheduledata(Endpoint):
     def get(self, req):
-        name = 'michael'#request.params.get('name', 'World')
-        return {'data': chain}
-        #return {'message': 'Hello, %s!' % name}
+        objects = Choices.objects.all().iterator()
+        dicted = [{'day': choice.day, 'block_length': choice.block_length, 'block_start': choice.block_start} for choice in objects]
+        something = []
+        for x in dicted:
+            start = x['block_start']
+            stop = start + x['block_length']
+            for i in range(start, stop):
+                something.append('{} {}'.format(x['day'], i))
+        return {'data': something}
